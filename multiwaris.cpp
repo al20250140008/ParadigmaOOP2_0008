12014e8a54fe66497e14799ac715c5dc097b0187
al20250140008 <alf80169@gmail.com>
2026-05-18 11:08:22 +0700

    menambahkan kelas 'orang' dengan konstruktor untuk menginisialisasi umur

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class orang {
+public:
+    int umur;
+    orang(int Pumur) : 
+         umur(Pumur) 
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+s
