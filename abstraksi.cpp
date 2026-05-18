56720a919a7fbf2c76309d8e6cbda5e5bf7bcb33
al20250140008 <alf80169@gmail.com>
2026-05-18 11:07:00 +0700

    menambahkan kelas 'AbstraksiKlas' dengan metode untuk mengisi dan menampilkan nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    // menampilkan 
+    void display() {
+        cout << "Nilai x: " << x << endl;
+        cout << "Nilai y: " << y << endl;
+    }
+};
